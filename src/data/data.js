03b812adf68d0967f1d84b5234665c9cb6f0ec0d
roman-yagodin <roman.yagodin@gmail.com>
2023-10-24 15:26:27 +0300

    Поддержка useSearchParams

--- src/data/data.js
+++ src/data/data.js
@@ -1,7 +1,8 @@
 const dataBook = [
   {"id":1, "title":"Book 1", "author":"Author 1", "price": 500},
   {"id":5, "title":"Book 2", "author":"Author 2", "price": 1200},
-  {"id":6, "title":"Book 3", "author":"Author 3", "price": 900}
+  {"id":6, "title":"Book 3", "author":"Author 3", "price": 900},
+  {"id":7, "title":"Koob 4", "author":"Rohtua 4", "price": 1}
 ];
 
 export default dataBook;
--- src/routes/catalog.jsx
+++ src/routes/catalog.jsx
@@ -1,24 +1,47 @@
-import {Outlet, NavLink} from 'react-router-dom';
+import {Outlet, NavLink, useSearchParams} from 'react-router-dom';
 import dataBook from '../data/data';
 
 export default function Catalog() {
+
+    const [searchParams, setSearchParams] = useSearchParams();
     return (
         <section>
             <h2>Каталог</h2>
-            {dataBook.map(book => (
-                <>
-                    <NavLink to={`/catalog/${book.id}`}
-                        style={({isActive}) => {
-                            return {
-                                color: isActive ? "#369" : "",
-                                fontWeight: isActive ? "bold" : "normal"       
-                            };
-                        }}
-                    >
-                        {book.title}
-                    </NavLink> { ' ' }
-                </>
-            ))}
+
+            <input type="search"
+                value={searchParams.get("filter") || ""}
+                onChange={e => {
+                    const filter = e.target.value;
+                    if (filter) {
+                        setSearchParams({filter})
+                    }
+                    else {
+                        setSearchParams({});
+                    }
+                }}
+            />
+
+            <nav>
+                {dataBook.filter(book => {
+                    const filter = searchParams.get("filter");
+                    if (!filter) return true;
+                    return book.title.toLocaleLowerCase().startsWith(filter.toLocaleLowerCase());
+
+                }).map(book => (
+                    <>
+                        <NavLink to={`/catalog/${book.id}`}
+                            style={({isActive}) => {
+                                return {
+                                    color: isActive ? "#369" : "",
+                                    fontWeight: isActive ? "bold" : "normal"       
+                                };
+                            }}
+                        >
+                            {book.title}
+                        </NavLink> { ' ' }
+                    </>
+                ))}
+            </nav>
             <Outlet />
         </section>
     );
